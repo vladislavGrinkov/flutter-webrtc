b1f307e5f8560dab3f37e239329c7b620ddf7071
Гриньков Владислав Леонидович <vladislav.grinkov@rtkit.ru>
2024-06-13 20:07:58 +0300

    updated new versions

--- README.md
+++ README.md
@@ -9,7 +9,11 @@ A few resources to get you started if this is your first Flutter project:
 
 - [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- Flutter - 3.16.9
 
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+
--- lib/screens/widgets/meeting_action_button.dart
+++ lib/screens/widgets/meeting_action_button.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../constants/styles.dart';
 
-
-
 // Action Button
 class MeetingActionButton extends StatelessWidget {
   final void Function()? onPressed;
@@ -24,21 +22,21 @@ class MeetingActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(radius),
-          border: Border.all(color: Colors.white30),
-          color: backgroundColor,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        border: Border.all(color: Colors.white30),
+        color: backgroundColor,
+      ),
+      padding: const EdgeInsets.all(8),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          size: iconSize,
+          color: iconColor,
         ),
-        padding: const EdgeInsets.all(8),
-        child: IconButton(
-          onPressed : onPressed,
-          icon : Icon(
-            icon,
-            size: iconSize,
-            color: iconColor,
-          ),
-        ),
-      );
+      ),
+    );
   }
 }
